afc164139aa78b50146b22c03de2f67c039fc70a
Pere Zamora <pere.zamora@atmira@com>
2018-04-04 22:06:57 +0200

    Filtro 3 campos del form show Alumnos para los tres formatos

--- Vueling.Presentation.Winsite/AlumnosShowForm.Designer.cs
+++ Vueling.Presentation.Winsite/AlumnosShowForm.Designer.cs
@@ -33,6 +33,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textGuid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,7 +47,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(67, 87);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(502, 259);
+            this.dataGridView1.Size = new System.Drawing.Size(619, 259);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -76,18 +82,73 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(494, 58);
+            this.button4.Location = new System.Drawing.Point(611, 34);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
             this.button4.Text = "search";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Guid";
+            // 
+            // textGuid
+            // 
+            this.textGuid.Location = new System.Drawing.Point(106, 36);
+            this.textGuid.Name = "textGuid";
+            this.textGuid.Size = new System.Drawing.Size(100, 20);
+            this.textGuid.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Id";
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(106, 61);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(100, 20);
+            this.textId.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nombre";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(278, 39);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(100, 20);
+            this.textNombre.TabIndex = 10;
             // 
             // AlumnosShowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textGuid);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -98,6 +159,7 @@
             this.Load += new System.EventHandler(this.AlumnosShowForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,5 +170,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textGuid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNombre;
     }
 }
--- Vueling.Presentation.Winsite/AlumnosShowForm.cs
+++ Vueling.Presentation.Winsite/AlumnosShowForm.cs
@@ -10,8 +10,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vueling.Business.Logic;
+using Vueling.Common.Logic.Enums;
 using Vueling.Common.Logic.Model;
 using Vueling.Common.Logic.Singletons;
+using Vueling.Common.Logic.Util;
 
 namespace Vueling.Presentation.Winsite
 {
@@ -20,8 +22,10 @@ namespace Vueling.Presentation.Winsite
 
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private IAlumnoBL alumnoBL;
+        private List<Alumno> listAlumnos;
         SingletonListaJson listaAlumnosJson;
         SingletonListaXml listaAlumnosXml;
+
 
         public AlumnosShowForm()
         {
@@ -35,7 +39,7 @@ namespace Vueling.Presentation.Winsite
             try
             {
                 log.Debug("Entrar AlumnosShowForm_Load: ");
-                List<Alumno> listAlumnos = alumnoBL.GetAlumnos();
+                listAlumnos = alumnoBL.GetAlumnos();
                 dataGridView1.ReadOnly = true;
                 dataGridView1.DataSource = listAlumnos;
             }
@@ -52,7 +56,8 @@ namespace Vueling.Presentation.Winsite
             log.Debug("Entrar Mostrar lista alumnos TXT: ");
             try
             {
-                List<Alumno> listAlumnos = alumnoBL.GetAlumnos();
+                ConfigUtils.SetValorVarEnvironment(Properties.Resources.FormatTxt);
+                listAlumnos = alumnoBL.GetAlumnos();
                 dataGridView1.ReadOnly = true;
                 dataGridView1.DataSource = listAlumnos;
             }
@@ -67,6 +72,7 @@ namespace Vueling.Presentation.Winsite
             log.Debug("Entrar Mostrar lista alumnos JSON: ");
             try
             {
+                ConfigUtils.SetValorVarEnvironment(Properties.Resources.FormatJson);
                 listaAlumnosJson = SingletonListaJson.Instance;
                 dataGridView1.ReadOnly = true;
                 dataGridView1.DataSource = listaAlumnosJson.ListaAlumnos;
@@ -82,6 +88,7 @@ namespace Vueling.Presentation.Winsite
             log.Debug("Entrar Mostrar lista alumnos XML: ");
             try
             {
+                ConfigUtils.SetValorVarEnvironment(Properties.Resources.FormatXml);
                 listaAlumnosXml = SingletonListaXml.Instance;
                 dataGridView1.ReadOnly = true;
                 dataGridView1.DataSource = listaAlumnosXml.ListaAlumnos;
@@ -90,6 +97,48 @@ namespace Vueling.Presentation.Winsite
             {
                 MessageBox.Show(ex.ToString(), "Error cargar Form", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            log.Debug("Entrar metodo Filtrar por campos");
+            List<Alumno> listAl;
+            var guid = textGuid.Text;
+            var id = textId.Text != "" ? Convert.ToInt32(textId.Text) : 0;
+            var Nombre = textNombre.Text;
+
+            switch (EnumApp.getValorFormatAlumno())
+            {
+                case EnumApp.OpcTypeFile.Txt:
+                    listAl = listAlumnos;
+                    break;
+                case EnumApp.OpcTypeFile.Json:
+                    listaAlumnosJson = SingletonListaJson.Instance;
+                    listAl = listaAlumnosJson.ListaAlumnos;
+                    break;
+                case EnumApp.OpcTypeFile.Xml:
+                    listaAlumnosXml = SingletonListaXml.Instance;
+                    listAl = listaAlumnosXml.ListaAlumnos;
+                    break;
+                default:
+                    listAl = listAlumnos;
+                    break;
+            }
+
+            var listFilter = from item in listAl
+                            where item.Id == id || item.Guid == guid || item.Name == Nombre
+                            select item;
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = listFilter.ToList<Alumno>();
+            ResetFieldForm();
+        }
+
+        private void ResetFieldForm()
+        {
+            textGuid.Text = "";
+            textId.Text = "";
+            textNombre.Text = "";
         }
     }
 }
