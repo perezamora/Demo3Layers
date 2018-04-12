7d9b902d46b68be76179988eb6757c4cede399ae
Pere Zamora <pere.zamora@atmira@com>
2018-04-12 22:29:06 +0200

    Proyecto sqlserver, tabla alumnos, control versiones DBA con flyway.

---
+++ Vueling.DataAcces.SqlLayer/Migration/V1__Init_Vueling_DB.sql
@@ -0,0 +1,17 @@
+﻿USE VuelingDB 
+IF OBJECT_ID('dbo.Alumnos', 'U') IS NOT NULL
+DROP TABLE dbo.Alumnos
+GO
+-- Creamos la Tabla
+CREATE TABLE dbo.Alumnos
+(
+   Id  				INT NOT NULL  IDENTITY(1,1) PRIMARY KEY, -- Clave Primaria
+   Guid             UNIQUEIDENTIFIER, 
+   Nombre     		[NVARCHAR](50)  NOT NULL,
+   Apellidos   		[NVARCHAR](50)  NOT NULL,
+   Dni         		[NVARCHAR](50)  NOT NULL,
+   FechaNacimiento  [DATETIME] NOT NULL, 
+   Edad				INT NOT NULL,
+   FechaCreacion	[NVARCHAR](50)  NOT NULL
+);
+GO
