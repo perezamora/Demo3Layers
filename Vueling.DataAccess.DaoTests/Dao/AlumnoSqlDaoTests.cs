6728e0902562faae206d52e52988981256874756
Pere Zamora Garcia <pere.zamora@atmira.com>
2018-04-17 20:06:21 +0200

    Implementar test integracion Crud - ISRT

---
+++ Vueling.DataAccess.DaoTests/Dao/AlumnoSqlDaoTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vueling.DataAcces.Dao.Dao;
+using System;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vueling.Common.Logic.Util;
+using System.Reflection;
+using Vueling.Common.Logic.Model;
+using System.Data;
+
+namespace Vueling.DataAcces.Dao.Dao.Tests
+{
+    [TestClass()]
+    public class AlumnoSqlDaoTests
+    {
+        private ICrudDao<Alumno> alumnoCrudDao;
+        private IDatabase database;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            alumnoCrudDao = new AlumnoSqlDao<Alumno>();
+            DBFactory factory = new DBFactory();
+            database = factory.DBSqlServer();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+
+        }
+
+        [DataRow("pere", "zamora", "3333", "01-03-1998", "20")]
+        [DataTestMethod]
+        public void InsertTest(string name, string apellidos, string dni, string fechaNac, string edad)
+        {
+
+            var countPreIsrt = this.CountAlumnosTable();
+
+            var lfechaNac = fechaNac.Split('-');
+            var FechaNac = new DateTime(Convert.ToInt32(lfechaNac[2]), Convert.ToInt32(lfechaNac[1]), Convert.ToInt32(lfechaNac[0]));
+
+            Alumno alumno = new Alumno(0, name, apellidos, dni, FechaNac, Convert.ToInt32(edad), DateTime.Now.ToString("yyyyMMddHHmmssffff"))
+            {
+                Guid = System.Guid.NewGuid().ToString()
+            };
+
+            Alumno alumnoIsrt = alumnoCrudDao.Insert(alumno);
+            var countPostIsrt = this.CountAlumnosTable();
+
+            Assert.IsTrue(countPreIsrt  < countPostIsrt);
+        }
+
+        private int CountAlumnosTable() {
+
+            try
+            {
+                using (IDbConnection connection = database.CreateOpenConnection())
+                {
+                    var sqlCommand = "SELECT COUNT(*) FROM ALUMNOS";
+                    using (IDbCommand command = database.CreateCommand(sqlCommand, connection))
+                    {
+                        return (Int32)command.ExecuteScalar();
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+
+}
